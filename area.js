db83dbb6eacfc8e5b8893aef4fcc6c61a7d278b6
bren <brendan.lough@gmail.com>
2012-09-02 00:28:39 -0700

    added comments

--- area.js
+++ area.js
@@ -1,6 +1,6 @@
 
 //basic area model
-var network = require('./spaceMath.js')
+var sm = require('./spaceMath.js')
 
 exports = module.exports = area;
 
@@ -11,13 +11,14 @@ function area() {
     this.bounds = {{-1,1},{1,-1}}; // top left to bottom righ
 }
 
-
+//add gameObject to current area
 area.prototype.addObject = function(obj){
     x = this.allObjects.push(obj); //add object to all object list
     if (obj.isGrav == true) this.gravitatingObjects.push(x); //add reference to object
 	if (obj.isRad == true) this.radiatingObjects.push(x); //add reference to object
 }
 
+//remove gameObject from current area
 area.prototype.removeObject = function(obj){
 	x = this.allObjects.indexOf(obj); //get index of object to remove
 	if (obj.isGrav == true){
@@ -31,16 +32,17 @@ area.prototype.removeObject = function(obj){
 	this.allObjects.splice(x,1); // remove object from array
 }
 
+//calc sum of all gravities at a point
 area.prototype.calcGrav = function(coords) {
 	//calc the grav contributions from all the objects in the solar system
 	var vec = {0,0};
 	for (grav in this.gravitatingObjects) {
-		vec = vectorAdd(vec,grav.calcGrav(coords)); // need to vector add this as it has direction and magnitude
+		vec = sm.vectorAdd(vec,grav.calcGrav(coords)); // need to vector add this as it has direction and magnitude
 	}
 	return vec;
 }
 
-
+//calc sum of all radiation at a point
 area.prototype.calcRad = function(coords) {
 	//this does not take into account occlusion so we should fix it eventually
 	//radiation does not have direction, only magnitude (lol)
@@ -51,11 +53,12 @@ area.prototype.calcRad = function(coords) {
 	return power;
 }
 
+//update the slow-changing properties of a scene like the effect of gravity from other planets etc
 area.prototype.updateSlow = function(amountOfTime) {
 	//update all the slow things
 	//ie calc grav updates to all objects in system
 	for (o in this.allObjects) {
-		o.updateHeading(this.calcGrav(o.coords));
+		o.applyImpulse(this.calcGrav(o.coords));
 		o.updateRad(this.calcRad(coords));
 	}
 }
