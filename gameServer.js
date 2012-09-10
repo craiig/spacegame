a1e1475cc9dfc259dd7f09b6162bb453393663fd
bren <brendan.lough@gmail.com>
2012-09-10 01:10:42 -0700

    more updates

--- gameServer.js
+++ gameServer.js
@@ -29,8 +29,8 @@ function GameServer(io) {
 	this.fileLoaded = false;
 	//register some callbacks - this is annoying to do this way, but our other options are way worse: http://www.dustindiaz.com/scoping-anonymous-functions/
 	var that = this
-	setInterval( function(){that.update()}, 100); //1 fps
-	setInterval( function(){that.slowUpdate()}, 100); //1 fps
+	setInterval( function(){that.update()}, 20); //1 fps
+	setInterval( function(){that.slowUpdate()}, 10); //1 fps
 
 	this.io.sockets.on('connection', function(socket){ that.newConnection(socket) });
 };
