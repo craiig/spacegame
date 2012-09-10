ec1591e35025e1319e71fb49c12ef968bc6cba8a
bren <brendanlough@hotmail.com>
2012-09-10 12:02:01 -0700

    fix broken build

--- client/world.js
+++ client/world.js
@@ -4,11 +4,12 @@ function World(socket){
 	this.socket = socket;
 	this.WorldData = {};// new Array(); //{};
 	this.network = new Network(this);
-
+	this.bounds = [[-2500,2500],[-2500,2500]];
+	this.midpoint=[0,0];
+	this.half = [2500,2500];
 	this.events = new EventEmitter();
 	this.events.on("init", function(){ console.log("world init"); });
 	this.events.on("update", function(){ console.log("world update"); });
-
 	var that = this
 
 	this.events.on("init", function(){
@@ -16,35 +17,32 @@ function World(socket){
 	})
 
 	this.events.on("update", function(){ //handle the update to the shiplist
+		var boundsFlag = false;
+		var tempBounds = that.bounds;
 		var items = [];
 		// Get a reference to the element.
 		var elem = document.getElementById('example');
 		if (elem && elem.getContext) {
 			var context = elem.getContext('2d');
   			if (context) {
+				//#ffffff
 				var colVal = 16777215;
 				var colInc = Math.floor(16777215 / Object.keys(that.WorldData).length);
 
 				for (obj in that.WorldData){
 					colVal -= colInc;								
 					var xobj = that.WorldData[obj];
-					if ((that.WorldData[obj].prevCoord!==undefined)&&(that.GameServerTime==undefined)){
+
+					//probably a better way to identify object types than this
+					if ((that.WorldData[obj].prevCoord!=undefined)&&(that.GameServerTime==undefined)){
 						if (
-							(that.WorldData[obj].prevCoord[0]>=-2500 )
+							(xobj.coords[0]>=that.bounds[0][0] ) 
 							&& 
-							(that.WorldData[obj].prevCoord[0]<=2500)
+							(xobj.coords[0]<=that.bounds[0][1] )
 							&&
-							(that.WorldData[obj].prevCoord[1]>=-2500 )
+							(xobj.coords[1]>=that.bounds[1][0]  ) 
 							&& 
-							(that.WorldData[obj].prevCoord[1]<=2500)
-							&&
-							(xobj.coords[0]>=-2500 ) 
-							&& 
-							(xobj.coords[0]<=2500)
-							&&
-							(xobj.coords[1]>=-2500 ) 
-							&& 
-							(xobj.coords[1]<=2500)
+							(xobj.coords[1]<=that.bounds[1][1])
 							) {
 								var cxx = colVal.toString(16);
 								while (cxx.length < 6){
@@ -52,22 +50,60 @@ function World(socket){
 								}
 								cxx =  "#"  +  cxx;
 								context.beginPath();
-								context.moveTo(that.WorldData[obj].prevCoord[0]+2500, that.WorldData[obj].prevCoord[1]+2500);	
-								context.lineTo((xobj.coords[0] +2500), (xobj.coords[1] + 2500) );
+								context.moveTo(that.WorldData[obj].prevCoord[0]+that.half[0], that.WorldData[obj].prevCoord[1]+that.half[1]);	
+								context.lineTo((xobj.coords[0] +that.half[0]), (xobj.coords[1] + that.half[1]) );
 								context.lineWidth = 1;
 								context.strokeStyle =cxx;
 								context.fillStyle ="#000000";
 								context.stroke();
-								context.rect((xobj.coords[0] +2500), (xobj.coords[1] + 2500),5,5);
+								context.rect((xobj.coords[0] +that.half[0]), (xobj.coords[1] + that.half[1]),5,5);
 								context.fill();
 								//context.strokeStyle ="#000000";
 								context.stroke();
 								that.WorldData[obj].prevCoord = xobj.coords; 
+							} else {
+								if (xobj.coords[0]<that.bounds[0][0])tempBounds[0][0]=Math.floor(xobj.coords[0]);
+								if (xobj.coords[0]>that.bounds[0][1])tempBounds[0][1]=Math.floor(xobj.coords[0]);
+								if (xobj.coords[1]<that.bounds[1][0])tempBounds[1][0]=Math.floor(xobj.coords[1]);
+								if (xobj.coords[1]>that.bounds[1][1])tempBounds[1][1]=Math.floor(xobj.coords[1]);
+								//boundsFlag=true;
 							}					
-						} else if (that.GameServerTime==undefined) {
-							that.WorldData[obj].prevCoord = xobj.coords; 
-						}
-					}
+						} else if ((that.GameServerTime==undefined)&&(that.WorldData[obj].prevCoord==undefined)) {
+							that.WorldData[obj].prevCoord = xobj.coords; 	
+						} 					}
+				}
+				console.log('b:' + tempBounds);
+				if (boundsFlag!=false) {
+					//console.log('bounds');
+					var tempHalf = [Math.floor((Math.abs(tempBounds[0][1])+Math.abs(tempBounds[0][0]))/2),
+								Math.floor(Math.abs((tempBounds[1][1])+Math.abs(tempBounds[1][0]))/2)];
+
+					var delta = [tempHalf[0] - that.half[0],tempHalf[1]-that.half[1]];
+
+//console.log(that.midpoint);
+//console.log(tempPoint);
+console.log('half:');
+console.log(that.half);
+					//that.midpoint = tempPoint;
+					that.half = tempHalf;
+
+					//var canvasData = '';
+					var canvasData = context.createImageData(elem.width, elem.height);
+					//console.log(elem.width);
+					//canvasData = context.getImageData(0, 0, elem.width, elem.height);
+					canvasData = context.getImageData(0, 0, elem.width, elem.height);
+					
+					//img.src = elem.toDataURL("image/png");
+//context.resize(delta[0],delta[1]);
+elem.width += delta[0];
+elem.height += delta[1];
+console.log(delta);
+console.log(elem);
+
+//context.drawImage(canvasData, delta[0], delta[1]);
+context.putImageData(canvasData, delta[0], delta[1]);
+
+boundsFlaf=false;
 				}
 			}	
 			for(ship in that.world.shipList){
