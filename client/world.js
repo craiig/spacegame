cd257ff40b9d2d2bba4b133dbfd887c008ccd81b
bren <brendan.lough@gmail.com>
2012-09-10 20:11:01 -0700

    less broken. Spacey!

--- client/world.js
+++ client/world.js
@@ -13,18 +13,18 @@ function World(socket){
 	this.isGfxInit = false;
 
 	var that = this
-	var elem1 = document.getElementById('example');
-		if (elem1 && elem1.getContext) {
-			var context1 = elem1.getContext('2d');
-  			if (context1) {
-				context1.fillStyle ="#000000";
-context.fillStyle   = '#CC5422'; // set canvas background color
-context.fillRect  (0,   0, 350, 350);  // now fill the canvas
-				context1.fillRect(0,0,elem1.width,elem1.height);
-				context1.fill();
+	// var elem1 = document.getElementById('example');
+	// 	if (elem1 && elem1.getContext) {
+	// 		var context1 = elem1.getContext('2d');
+ //  			if (context1) {
+	// 			context1.fillStyle ="#000000";
+	// 			context.fillStyle   = '#CC5422'; // set canvas background color
+	// 			context.fillRect  (0,   0, 350, 350);  // now fill the canvas
+	// 			context1.fillRect(0,0,elem1.width,elem1.height);
+	// 			context1.fill();
 
-			}
-		}
+	// 		}
+	// 	}
 
 
 
@@ -33,21 +33,23 @@ context.fillRect  (0,   0, 350, 350);  // now fill the canvas
 	})
 
 	this.events.on("update", function(){ //handle the update to the shiplist
-		var boundsFlag = false;
-		var tempBounds = that.bounds;
+		//var boundsFlag = false;
+		//var tempBounds = that.bounds;
 		var items = [];
 		// Get a reference to the element.
-		var elem = document.getElementById('example');
-		if (elem && elem.getContext) {
-			var context = elem.getContext('2d');
-  			if (context) {
-				if (that.isGfxInit==false) {
-					context.fillRect(0,0,elem.width,elem.height);
-					that.isGfxInit=true;
-				}
+// 		var elem = document.getElementById('example');
+// 		if (elem && elem.getContext) {
+// 			var context = elem.getContext('2d');
+//   			if (context) {
+// 				if (that.isGfxInit==false) {
+// 					context.fillRect(0,0,elem.width,elem.height);
+// 					that.isGfxInit=true;
+// 				}
 
 
-				//#ffffff
+
+
+//#ffffff
 				var colVal = 16777215;
 				var colInc = Math.floor(16777215 / Object.keys(that.WorldData).length);
 
@@ -55,8 +57,14 @@ context.fillRect  (0,   0, 350, 350);  // now fill the canvas
 					colVal -= colInc;								
 					var xobj = that.WorldData[obj];
 
+					if (that.GameServerTime!==undefined) {
+						console.log(that.WorldData[obj]);
+
+					}
+
+
 					//probably a better way to identify object types than this
-					if ((that.WorldData[obj].prevCoord!=undefined)&&(that.GameServerTime==undefined)){
+					if ((that.WorldData[obj].prevCoord!=undefined)&&(that.WorldData[obj].GameServerTime==undefined)){
 						if (
 							(xobj.coords[0]>=that.bounds[0][0] ) 
 							&& 
@@ -71,68 +79,102 @@ context.fillRect  (0,   0, 350, 350);  // now fill the canvas
 										cxx = "0" + cxx;
 								}
 								cxx =  "#"  +  cxx;
-								context.fillStyle =cxx;
-								context.fillRect(that.WorldData[obj].prevCoord[0]+that.half[0], that.WorldData[obj].prevCoord[1]+that.half[1],5,5);	
+								}
+							}
+						}
 
-								context.beginPath();
-								context.moveTo(that.WorldData[obj].prevCoord[0]+that.half[0], that.WorldData[obj].prevCoord[1]+that.half[1]);	
-								context.lineTo((xobj.coords[0] +that.half[0]), (xobj.coords[1] + that.half[1]) );
-								context.lineWidth = 1;
-								context.strokeStyle =cxx;
-								context.fillStyle ="#000000";
-								context.stroke();
-								context.rect((xobj.coords[0] +that.half[0]), (xobj.coords[1] + that.half[1]),5,5);
-								context.fill();
-								//context.strokeStyle ="#000000";
-								context.stroke();
-								that.WorldData[obj].prevCoord = xobj.coords; 
-							} else {
-								if (xobj.coords[0]<that.bounds[0][0])tempBounds[0][0]=Math.floor(xobj.coords[0]);
-								if (xobj.coords[0]>that.bounds[0][1])tempBounds[0][1]=Math.floor(xobj.coords[0]);
-								if (xobj.coords[1]<that.bounds[1][0])tempBounds[1][0]=Math.floor(xobj.coords[1]);
-								if (xobj.coords[1]>that.bounds[1][1])tempBounds[1][1]=Math.floor(xobj.coords[1]);
-								//boundsFlag=true;
-							}					
-						} else if ((that.GameServerTime==undefined)&&(that.WorldData[obj].prevCoord==undefined)) {
-							that.WorldData[obj].prevCoord = xobj.coords; 	
-						} 					}
-				}
+
+
+
+
+
+
+// 				//#ffffff
+// 				var colVal = 16777215;
+// 				var colInc = Math.floor(16777215 / Object.keys(that.WorldData).length);
+
+// 				for (obj in that.WorldData){
+// 					colVal -= colInc;								
+// 					var xobj = that.WorldData[obj];
+
+// 					//probably a better way to identify object types than this
+// 					if ((that.WorldData[obj].prevCoord!=undefined)&&(that.GameServerTime==undefined)){
+// 						if (
+// 							(xobj.coords[0]>=that.bounds[0][0] ) 
+// 							&& 
+// 							(xobj.coords[0]<=that.bounds[0][1] )
+// 							&&
+// 							(xobj.coords[1]>=that.bounds[1][0]  ) 
+// 							&& 
+// 							(xobj.coords[1]<=that.bounds[1][1])
+// 							) {
+// 								var cxx = colVal.toString(16);
+// 								while (cxx.length < 6){
+// 										cxx = "0" + cxx;
+// 								}
+// 								cxx =  "#"  +  cxx;
+// 								context.fillStyle =cxx;
+// 								context.fillRect(that.WorldData[obj].prevCoord[0]+that.half[0], that.WorldData[obj].prevCoord[1]+that.half[1],5,5);	
+
+// 								context.beginPath();
+// 								context.moveTo(that.WorldData[obj].prevCoord[0]+that.half[0], that.WorldData[obj].prevCoord[1]+that.half[1]);	
+// 								context.lineTo((xobj.coords[0] +that.half[0]), (xobj.coords[1] + that.half[1]) );
+// 								context.lineWidth = 1;
+// 								context.strokeStyle =cxx;
+// 								context.fillStyle ="#000000";
+// 								context.stroke();
+// 								context.rect((xobj.coords[0] +that.half[0]), (xobj.coords[1] + that.half[1]),5,5);
+// 								context.fill();
+// 								//context.strokeStyle ="#000000";
+// 								context.stroke();
+// 								that.WorldData[obj].prevCoord = xobj.coords; 
+// 							} else {
+// 								if (xobj.coords[0]<that.bounds[0][0])tempBounds[0][0]=Math.floor(xobj.coords[0]);
+// 								if (xobj.coords[0]>that.bounds[0][1])tempBounds[0][1]=Math.floor(xobj.coords[0]);
+// 								if (xobj.coords[1]<that.bounds[1][0])tempBounds[1][0]=Math.floor(xobj.coords[1]);
+// 								if (xobj.coords[1]>that.bounds[1][1])tempBounds[1][1]=Math.floor(xobj.coords[1]);
+// 								//boundsFlag=true;
+// 							}					
+// 						} else if ((that.GameServerTime==undefined)&&(that.WorldData[obj].prevCoord==undefined)) {
+// 							that.WorldData[obj].prevCoord = xobj.coords; 	
+// 						} 					}
+// 				}
 
 				
-				console.log('b:' + tempBounds);
-				if (boundsFlag!=false) {
-					//console.log('bounds');
-					var tempHalf = [Math.floor((Math.abs(tempBounds[0][1])+Math.abs(tempBounds[0][0]))/2),
-								Math.floor(Math.abs((tempBounds[1][1])+Math.abs(tempBounds[1][0]))/2)];
+// 				console.log('b:' + tempBounds);
+// 				if (boundsFlag!=false) {
+// 					//console.log('bounds');
+// 					var tempHalf = [Math.floor((Math.abs(tempBounds[0][1])+Math.abs(tempBounds[0][0]))/2),
+// 								Math.floor(Math.abs((tempBounds[1][1])+Math.abs(tempBounds[1][0]))/2)];
 
-					var delta = [tempHalf[0] - that.half[0],tempHalf[1]-that.half[1]];
+// 					var delta = [tempHalf[0] - that.half[0],tempHalf[1]-that.half[1]];
 
-//console.log(that.midpoint);
-//console.log(tempPoint);
-console.log('half:');
-console.log(that.half);
-					//that.midpoint = tempPoint;
-					that.half = tempHalf;
+// //console.log(that.midpoint);
+// //console.log(tempPoint);
+// console.log('half:');
+// console.log(that.half);
+// 					//that.midpoint = tempPoint;
+// 					that.half = tempHalf;
 
-					//var canvasData = '';
-					var canvasData = context.createImageData(elem.width, elem.height);
-					//console.log(elem.width);
-					//canvasData = context.getImageData(0, 0, elem.width, elem.height);
-					canvasData = context.getImageData(0, 0, elem.width, elem.height);
+// 					//var canvasData = '';
+// 					var canvasData = context.createImageData(elem.width, elem.height);
+// 					//console.log(elem.width);
+// 					//canvasData = context.getImageData(0, 0, elem.width, elem.height);
+// 					canvasData = context.getImageData(0, 0, elem.width, elem.height);
 					
-					//img.src = elem.toDataURL("image/png");
-//context.resize(delta[0],delta[1]);
-elem.width += delta[0];
-elem.height += delta[1];
-console.log(delta);
-console.log(elem);
+// 					//img.src = elem.toDataURL("image/png");
+// //context.resize(delta[0],delta[1]);
+// elem.width += delta[0];
+// elem.height += delta[1];
+// console.log(delta);
+// console.log(elem);
 
-//context.drawImage(canvasData, delta[0], delta[1]);
-context.putImageData(canvasData, delta[0], delta[1]);
+// //context.drawImage(canvasData, delta[0], delta[1]);
+// context.putImageData(canvasData, delta[0], delta[1]);
 
-boundsFlaf=false;
-				}
-			}	
+// boundsFlaf=false;
+// 				}
+// 			}	
 			for(ship in that.world.shipList){
 				so = that.world.shipList[ship];
 				var shiphtml = "<li>"+so.name+" x: "+ so.x+" y:"+so.y+ " rot:"+so.rot;
