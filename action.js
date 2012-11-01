1ec30252faf8cda00e736eb298c0aa2918d6edba
bren <brendan.lough@gmail.com>
2012-11-01 12:57:37 -0700

    action updates

--- action.js
+++ action.js
@@ -5,26 +5,27 @@
 exports = module.exports = action;
 
 action.prototype.actionType = {
-    createGroup:{},
-	createShip:{},
-	moveShip:{to:{}},
-	moveGroup:{to:{}},
-	trade:{from:{},to:{},haggle:false,cargo:{}}
-	
-}
-function action(world) {
-    this.allObjects = new Array(); //all objects in a level / physicalArea
-	this.gravitatingObjects =  new Array(); //just index into allObjects
-	this.radiatingObjects = new Array(); //just index into allObjects
-    this.bounds = new Array(); // top left to bottom righ
-    this.playerShips = new Array();
-    this.world = world;
+    createShipGroup:{groupType:Object}
+    ,destroyShipGroup:{group:Object}
+    ,createBuilding:{cell:Object,buildingType:Number}
+    ,destroyBuilding:{cell:Object}
+    ,createShip:{shipType:Number}
+    ,destroyShip:{ship:Object}
+    ,moveShip:{ship:Object,to:Object}
+    ,moveGroup:{group:Object,to:Object}
+    ,trade:{from:Object,to:Object,haggle:Boolean,cargo:Object}
+	,doResearch:{researchTopic:Object}
+	,declareWar:{civ:Object}
+	,proposeTreaty:{civ:Object,terms:Object}
+	,acceptTreaty:{civ:Object}
+	,rejectTreaty:{civ:Object}
+	,attack:{attackType:Object}
 }
 
-//add gameObject to current physicalArea
+function action(world) {
+
+}
+
 action.prototype.addObject = function(obj){
-    x = this.allObjects.push(obj); //add object to all object list
-    if (obj.isGrav == true) this.gravitatingObjects.push(x); //add reference to object
-	if (obj.isRad == true) this.radiatingObjects.push(x); //add reference to object
-	if (obj.isPlayerShip == true) this.playerShips.push(x); //add reference to object
+
 }
